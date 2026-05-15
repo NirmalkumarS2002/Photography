d94ead5b05112565f2e8ffbad2d30dd7bfee4a2e
NirmalkumarS2002 <nirmalsiva555@gmail.com>
2026-05-15 16:13:02 +0530

    nav bar alignment done

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 let arrowbtn=document.querySelector(".arrowdown1")
 let page=document.querySelector(".pages")
 
@@ -24,5 +26,50 @@ arrowbtn2.addEventListener("click",(e)=>{
     }
     else{
         contactlink.style.display="flex"
+    }
+})
+
+//toggle button
+
+
+let toggle=document.querySelector(".toggle")
+let nav=document.querySelector("nav")
+let close1=document.querySelector(".close")
+
+toggle.addEventListener("click",()=>{
+
+    toggle.style.display="none"
+    close1.style.display="block"
+    nav.style.display="flex"
+      page.style.display="none"
+    contactlink.style.display="none"
+
+})
+
+close1.addEventListener("click",()=>{
+
+    toggle.style.display="block"
+    close1.style.display="none"
+    nav.style.display="none"
+    page.style.display="none"
+    contactlink.style.display="none"
+
+})
+
+
+window.addEventListener("resize",()=>{
+
+
+    if(innerWidth>1050){
+         close1.style.display="none"     
+         toggle.style.display="none"
+         nav.style.display="flex"
+    }
+    else{
+        close1.style.display="none"     
+         toggle.style.display="block"
+         nav.style.display="none"
+         
+
     }
 })
